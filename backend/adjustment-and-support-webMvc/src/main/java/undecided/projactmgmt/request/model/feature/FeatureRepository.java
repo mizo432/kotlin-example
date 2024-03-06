2661a20799e8211d678044584b98d7c7826c94d4
mizoguchi kenichi <hachirouuemon.mizoguchi@gmail.com>
2024-03-06 18:40:47 +0900

    feat ( FeatureRepository.java ): add FeatureRepository.

---
+++ backend/adjustment-and-support-webMvc/src/main/java/undecided/projactmgmt/request/model/feature/FeatureRepository.java
@@ -0,0 +1,6 @@
+package undecided.projactmgmt.request.model.feature;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FeatureRepository extends JpaRepository<Feature, Long> {
+}
