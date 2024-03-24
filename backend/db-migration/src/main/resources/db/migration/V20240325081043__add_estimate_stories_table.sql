cba05766257a77872b62a164f9c055845649577a
mizoguchi kenichi <hachirouuemon.mizoguchi@gmail.com>
2024-03-25 08:19:30 +0900

    :change

--- backend/db-migration/src/main/resources/db/migration/V20240325081043__add_estimate_stories_table.sql
+++ backend/db-migration/src/main/resources/db/migration/V20240325081043__add_estimate_stories_table.sql
@@ -1,10 +1,8 @@
 create table adjustment.estimate_stories
 (
-    estimate_story_id               bigInt,
-    story_id                        bigInt    not null,
-    occurred_at                     timestamp not null,
-    rank                            INTEGER,
-    fully_implemented_satisfactions varchar(20),
-    not_implemented_satisfactions   varchar(20),
+    estimate_story_id bigInt,
+    story_id          bigInt    not null,
+    occurred_at       timestamp not null,
+    effort            INTEGER   not null,
     primary key (estimate_story_id)
 );
